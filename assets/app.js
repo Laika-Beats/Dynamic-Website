60dcb0ffa8d5bb4bf2925ec5b13a50e1fec8023d
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-01 11:04:53 -0700

    new page

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+const burger = document.querySelector(".burger");
 
 function animateSlides() {
   // init controller
@@ -87,7 +88,29 @@ function activeCursor(event) {
     gsap.to(".title-swipe", 1, { y: "100%" });
   }
 }
+function navToggle(event) {
+  const line3 = document.querySelector(".line3");
+  if (!event.target.classList.contains("active")) {
+    event.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    line3.classList.add("active");
+    document.body.classList.add("hide");
+  } else {
+    event.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    line3.classList.remove("active");
+    document.body.classList.remove("hide");
+  }
+}
 
+// EventListeners
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
 
